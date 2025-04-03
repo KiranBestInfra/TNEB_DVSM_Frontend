c9ac1641654a06836494cba5f600b59c1f929408
VSH Kiran <saiharikiran@outlook.com>
2025-04-03 20:03:59 +0530

    base name changed

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/bi/',
+    base: '/exedb/',
     plugins: [react()],
     define: {
         __WS_TOKEN__: `"${process.env.VITE_WS_TOKEN}"`,
