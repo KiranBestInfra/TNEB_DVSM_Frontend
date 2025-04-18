11336cd692b2119d3bee2c4fd59f7c802ac41ccc
Madhu <madhu.bestinfra@gmail.com>
2025-04-18 17:25:52 +0530

    Updated Datepicker for graphs

--- src/pages/EdcDetails.jsx
+++ src/pages/EdcDetails.jsx
@@ -14,11 +14,13 @@ import { useAuth } from '../components/AuthProvider';
 const EdcDetails = () => {
     const { edc: edcParam } = useParams();
     const { user, isCircle } = useAuth();
+    console.log('user', user);
     const edcName = isCircle() && user?.name ? user.name : edcParam;
     const { region, edcId: paramEdcId } = useParams();
     const edcId = isCircle() ? user?.hierarchy_id : paramEdcId;
-    const { isRegion } = useAuth();
+    const { isRegion, isAdmin } = useAuth();
     const regionUser = isRegion();
+    const adminUser = isAdmin();
     const [timeRange, setTimeRange] = useState('Daily');
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [graphData, setGraphData] = useState({
@@ -73,7 +75,7 @@ const EdcDetails = () => {
 
     //const entityId =  user?.id;
     const entityId = isCircle() ? user?.id : (regionUser ? edcId : edcId);
-    const entityName = regionUser ? entityId : edcName?.replace('_EDC', '').toLowerCase();
+    const entityName = adminUser || regionUser ? entityId : edcName?.replace('_EDC', '').toLowerCase();
     //console.log('entityName', entityName);
     const navigate = useNavigate();
 
